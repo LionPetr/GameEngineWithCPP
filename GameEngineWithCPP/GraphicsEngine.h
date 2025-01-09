9baa9836890bda54feb0f4c1c3d70f93b3bc4ef5
Lion <lionpetrenko@gmail.com>
2025-01-08 18:35:05 -0800

    setting up vertex buffer and triangle works

--- GameEngineWithCPP/GraphicsEngine.h
+++ GameEngineWithCPP/GraphicsEngine.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <d3d11.h>
+#include <d3dcompiler.h>
 
 class SwapChain;
+class VertexBuffer;
 class DeviceContext;
 class GraphicsEngine
 {
@@ -17,9 +19,14 @@ public:
 
 	static GraphicsEngine* get();
 
+	bool createShaders();
+	bool setShaders();
+	void getShaderBufferAndSize(void** bytecode, UINT* size);
+
 	SwapChain* createSwapChain();
 	//DeviceContext* getImmidiateDeviceContext();
 	DeviceContext* getImmidiateDeviceContext();
+	VertexBuffer* createVertexBuffer();
 private:
 	DeviceContext* m_imm_device_context;
 
@@ -32,8 +39,15 @@ private:
 	IDXGIDevice* m_dxgi_device;
 	IDXGIAdapter* m_dxgi_adapter;
 	IDXGIFactory* m_dxgi_factory;
+	ID3D11DeviceContext* m_imm_context;
+
+	ID3DBlob* m_vsblob = nullptr;
+	ID3DBlob* m_psblob = nullptr;
+	ID3D11VertexShader* m_vs = nullptr;
+	ID3D11PixelShader* m_ps = nullptr;
 
 	friend class SwapChain;
+	friend class VertexBuffer;
 
 };
 
