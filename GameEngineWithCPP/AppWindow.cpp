e57698a6d8863ab43ca7ea5bf580a18244ffb782
Lion <lionpetrenko@gmail.com>
2025-01-18 21:50:07 -0800

    added a camera mechanic, still dont understand how some of it works *Check it later*

--- GameEngineWithCPP/AppWindow.cpp
+++ GameEngineWithCPP/AppWindow.cpp
@@ -27,7 +27,7 @@ AppWindow::AppWindow()
 {
 }
 
-void AppWindow::updateQuadPosition()
+void AppWindow::update()
 {
 	constant cc;
 	cc.m_time = ::GetTickCount64();
@@ -52,7 +52,7 @@ void AppWindow::updateQuadPosition()
 
 	//cc.m_world *= temp;
 
-	cc.m_world.setScale(Vector3D(m_scale_cube, m_scale_cube, m_scale_cube));
+	/*cc.m_world.setScale(Vector3D(m_scale_cube, m_scale_cube, m_scale_cube));
 
 	temp.setIdentity();
 	temp.setRoationZ(0.0f);
@@ -64,20 +64,48 @@ void AppWindow::updateQuadPosition()
 
 	temp.setIdentity();
 	temp.setRoationX(m_rot_x);
-	cc.m_world *= temp;
+	cc.m_world *= temp; */
+
+	cc.m_world.setIdentity();
+
+	Matrix4x4 world_cam;
+	world_cam.setIdentity();
+
+
+	temp.setIdentity();
+	temp.setRoationX(m_rot_x);
+	world_cam *= temp;
+
+	temp.setIdentity();
+	temp.setRoationY(m_rot_y);
+	world_cam *= temp;
+
+	Vector3D new_pos = m_world_cam.getTranslation() + world_cam.getZDirection() * (m_forward * 0.3f);
+
+	new_pos = new_pos + world_cam.getXDirection() * (m_rightward * 0.3f);
+
+	world_cam.setTranslation(new_pos);
+
+	m_world_cam = world_cam;
+	world_cam.inverse();
 
 
 
 
-	cc.m_view.setIdentity();
-	cc.m_proj.setOrthoLH
-	(
-		(this->getClientWindowRect().right - this->getClientWindowRect().left) / 400.0f,
-		(this->getClientWindowRect().bottom - this->getClientWindowRect().top) / 400.0f,
-		-4.0f,
-		4.0f
-	);
+	cc.m_view = world_cam;
+	//cc.m_proj.setOrthoLH
+	//(
+	//	(this->getClientWindowRect().right - this->getClientWindowRect().left) / 400.0f,
+	//	(this->getClientWindowRect().bottom - this->getClientWindowRect().top) / 400.0f,
+	//	-4.0f,
+	//	4.0f
+	//);
 	
+	int width = (this->getClientWindowRect().right - this->getClientWindowRect().left);
+	int height = (this->getClientWindowRect().bottom - this->getClientWindowRect().top);
+
+	cc.m_proj.setPerspectiveFovLH(1.57f, ((float)width / (float)height), 0.1f, 100.0f);
+
 	m_cb->update(GraphicsEngine::get()->getImmidiateDeviceContext(), &cc);
 
 	
@@ -92,12 +120,15 @@ void AppWindow::onCreate()
 	Window::onCreate();
 
 	InputSystem::get()->addListener(this);
+	InputSystem::get()->showCursor(false);
 
 	GraphicsEngine::get()->init();
 	m_swap_chain = GraphicsEngine::get()->createSwapChain();
 
 	RECT rc = this->getClientWindowRect();
 	m_swap_chain->init(this->m_hwnd, rc.right - rc.left, rc.bottom - rc.top);
+
+	m_world_cam.setTranslation(Vector3D(0, 0, -2));
 
 	vertex vertex_list[] =
 	{
@@ -180,7 +211,7 @@ void AppWindow::onUpdate()
 	GraphicsEngine::get()->getImmidiateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
 
 
-	updateQuadPosition();
+	update();
 
 
 	GraphicsEngine::get()->getImmidiateDeviceContext()->setConstantBuffer(m_vs, m_cb);
@@ -225,33 +256,41 @@ void AppWindow::onKillFocus()
 
 void AppWindow::onKeyDown(int key)
 {
-
+	if (key == 'W')
+	{
+		m_forward = 1.0f;
+	}
+	else if (key == 'S')
+	{
+		m_forward = -1.0f;
+	}
+	else if (key == 'A')
+	{
+		m_rightward = -1.0f;
+	}
+	else if (key == 'D')
+	{
+		m_rightward = 1.0f;
+	}
 }
 
 void AppWindow::onKeyUp(int key)
 {
-	if (key == 'W')
-	{
-		m_rot_x += 3.14f * m_delta_time;
-	}
-	else if (key == 'S')
-	{
-		m_rot_x -= 3.14f * m_delta_time;
-	}
-	else if (key == 'A')
-	{
-		m_rot_y += 3.14f * m_delta_time;
-	}
-	else if (key == 'D')
-	{
-		m_rot_y -= 3.14f * m_delta_time;
-	}
+	m_forward = 0.0f;
+	m_rightward = 0.0f;
 }
 
-void AppWindow::onMouseMove(const Point& delta_mouse_pos)
+void AppWindow::onMouseMove(const Point& mouse_pos)
 {
-	m_rot_x -= delta_mouse_pos.m_y * m_delta_time;
-	m_rot_y -= delta_mouse_pos.m_x * m_delta_time;
+	int width = (this->getClientWindowRect().right - this->getClientWindowRect().left);
+	int height = (this->getClientWindowRect().bottom - this->getClientWindowRect().top);
+
+
+	m_rot_x += (mouse_pos.m_y - (height / 2.0f)) * m_delta_time * 0.1f;
+	m_rot_y += (mouse_pos.m_x - (width / 2.0f)) * m_delta_time * 0.1f;
+
+
+	InputSystem::get()->setCursorPosition(Point(width / 2.0f, height / 2.0f));
 
 }
 
